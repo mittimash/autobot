3674704df291acc220bab62d829fd9bc7bc18a7c
Dmitriy Chernov <36071632+mittimash@users.noreply.github.com>
2025-04-01 00:11:02 +0700

    t
    new

--- autobot.ino
+++ autobot.ino
@@ -1,53 +1,135 @@
-// объявление пинов для компонентов
-#define SERVO_PIN 11
-#define HC_TRIG 12
-#define HC_ECHO 10
-
-//объявление библиотек
-#include <Servo.h> 
 #include <NewPing.h>
+#include "Servo.h"
 
-// объявление переменных компонентов машинки
-Servo myservo;
-NewPing sonar(HC_TRIG, HC_ECHO, 100);
+Servo servo;
 
-// активация пинов и компонентов, преднастройки
-void setup() { 
-  myservo.attach(SERVO_PIN);
+#define PIN_TRIG 13
+#define PIN_ECHO 12
+#define MAX_DISTANCE 300 
+NewPing sonar(PIN_TRIG, PIN_ECHO, MAX_DISTANCE);
+
+int in1 = 7;
+int in2 = 6;
+int in3 = 5;
+int in4 = 4;
+int r = 3;
+int L = 10;
+
+int mils;
+
+
+void setup() {
   Serial.begin(9600);
-  Serial.println('Hello!');
-} 
+  servo.attach(11);
+  //ставим серву на 90 градусов (прямо)
+  servo.write(90);
 
-// главная функция робота
-void loop() {
-  int where = servo_move();
-  Serial.println(where);
-  delay(50);
-} 
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(r, OUTPUT);
+  pinMode(L, OUTPUT);
+}
 
-// функция для сбора данных справа и слева от машинки 
-// поворачивает голову сервы и собирает данные о расстоянии 
-// возвращает направление куда можно повернуть машинке и ехать. 
-// 0 - направо, 
-// 1 - налево
+void test() {
 
 
-int servo_move() {
-  myservo.write(30);
-  int right = sonar.ping_cm();
-  Serial.print('Right: ');
-  Serial.println(right);
-  delay(2000);
-  myservo.write(180);
-  int left = sonar.ping_cm();
-  Serial.print('left: ');
-  Serial.println(left);
-  delay(2000);
-  myservo.write(110);
-  if (right > left) {
-    return 0;
-  }
-  return 1;
+
+}
+
+void backward() {
+  stop();
+  delay(100);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  digitalWrite(r, HIGH);
+  digitalWrite(L, HIGH);
+}
+
+void stop() {
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, HIGH);
+  digitalWrite(r, HIGH);
+  digitalWrite(L, HIGH);
+}
+
+void left(int mils) {
+  stop();
+  delay(500);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  digitalWrite(r, HIGH);
+  digitalWrite(L, HIGH);
+  delay(mils);
+  stop();
 }
 
 
+void right(int mils) {
+  stop();
+  delay(500);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  digitalWrite(r, HIGH);
+  digitalWrite(L, HIGH);
+  delay(mils);
+  stop();
+}
+
+void forward() {
+  stop();
+  delay(500);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  digitalWrite(L, HIGH);
+  digitalWrite(r, HIGH);
+}
+
+
+
+void loop() {
+  delay(30);
+  int forward_free = sonar.ping_cm();
+  forward();
+  while (forward_free > 30 || forward_free == 0) {
+      delay(30);
+      forward_free = sonar.ping_cm();
+      delay(30);
+    }
+  stop();
+  int side = servo_move();
+  if (side == 1) {left(280);}
+  if (side == 0) {right(280);}
+ 
+}
+
+int servo_move() {
+  servo.write(180);
+  delay(500);
+  int left = sonar.ping_cm();
+  delay(1000);
+
+  servo.write(0);
+  delay(500);
+  int right = sonar.ping_cm();
+  delay(1000);
+
+  servo.write(90);
+  delay(500);
+  if (left > right || left == 0) {
+    return 1;
+    } 
+  
+  return 0;
+}
